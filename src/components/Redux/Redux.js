9c7b6e6ad2d622237975a17147c1eb4214869551
nicoarias47 <ariasn1995@gmail.com>
2022-09-14 22:19:49 -0300

    starting with redux

---
+++ src/components/Redux/Redux.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "../../features/counter/counter";
+
+const Redux = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default Redux;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Redux from "../components/Redux/Redux";
 
 const Home = () => {
-  return <div style={{ color: "red", fontSize: "2rem" }}>Home</div>;
+  return (
+    <div style={{ color: "red", fontSize: "2rem" }}>
+      <p>Home</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Redux />
+    </div>
+  );
 };
 
 export default Home;
