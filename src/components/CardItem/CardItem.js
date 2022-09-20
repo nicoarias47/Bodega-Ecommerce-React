1adfeedbe2a753287da89c3cebd1cb797e10ad0b
nicoarias47 <ariasn1995@gmail.com>
2022-09-20 16:44:20 -0300

    media queries general

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./CardItem.css";
@@ -7,20 +7,18 @@ import "./CardItem.css";
 function CardItem({ data }) {
   return (
     <>
-      <Col className="d-flex align-items-stretch">
-        <Card>
-          <Link to={`/detalle/${data.id}`}>
-            <Card.Img variant="top" src={data.img} />
+      <Card className="d-flex align-items-stretch">
+        <Link to={`/detalle/${data.id}`}>
+          <Card.Img variant="top" src={data.img} />
+        </Link>
+        <Card.Body>
+          <Card.Title className="card-title">{data.name}</Card.Title>
+          <Card.Text className="card-variety">{data.variety}</Card.Text>
+          <Link to={`/detalle/${data.id}`} style={{ textDecoration: "none" }}>
+            <Card.Text className="see-detail">Ver mas</Card.Text>
           </Link>
-          <Card.Body>
-            <Card.Title className="card-title">{data.name}</Card.Title>
-            <Card.Text className="card-variety">{data.variety}</Card.Text>
-            <Link to={`/detalle/${data.id}`} style={{ textDecoration: "none" }}>
-              <Card.Text className="see-detail">Ver mas</Card.Text>
-            </Link>
-          </Card.Body>
-        </Card>
-      </Col>
+        </Card.Body>
+      </Card>
     </>
   );
 }
